22c394f7456f947d076601b6322d22c86b2a1ece
ioanniswd <ioanniswd@gmail.com>
2017-01-16 16:17:05 +0200

    url confirmation works. can check if url already exists. problem with res.send(). asynchronous. leaves it undefined and sends nothing

--- app/urlshort.js
+++ app/urlshort.js
@@ -1,45 +1,90 @@
 'use strict';
 
 module.exports = function(app) {
-	var validUrl = require('valid-url');
-	var mongo = require('mongodb');
+	var mongo = require('mongodb').MongoClient;
 	var url = 'mongodb://localhost';
-	var insertDocs = function(db) {
-		var collection = db.collection('urls');
-		collection.insertMany([{a:1}, {a:2}, {a:3} ]);
-	};
+	var givenUrl;
 	
-	mongo.MongoClient.connect(url, function(err, db) {	
+	mongo.connect(url, function(err, db) {	
+		if(err) throw err;
 		console.log('Connected to server');
-		insertDocs(db);
-		
-		console.log(db.listCollections());
 	db.close;
 	});
 	
 	app.route('/:url')
 	.get(function(req, res) {
 		//check if exists
-		//if exists redirect else send message
+		mongo.connect(url, function(err, db){
+			if(err) throw err;
+			var collection = db.collection("urls");
+			
+			//if exists redirect else send message
+			collection.findOne({
+				"original": process.argv[2]
+			}, function(err, result) {
+				if(err) throw err;
+				
+				if(result) {
+					console.log(result);
+					res.end("Finished");
+				} else {
+					res.send("This url is not in the databse");
+				}
+			});
+			
+			db.close();
+		});
+		
 	});
 	
-	app.route('/new/:url')
+	app.route('/new/:url*')
 	.get(function(req, res) {
-		var url = req.originalUrl;
-		console.log(url);
+		
+		var temp;
+		var clientUrl = req.originalUrl.substring(5);
+		console.log(clientUrl);
 		//check if valid
-		if(validUrl.isUri(url)) {
+		if(validUrl(clientUrl)) {
 			console.log("valid");
 			//if valid check if exists
-			//if not create short url
+			mongo.connect(url, function(err, db) {
+				if(err) throw err;
+				var collection = db.collection("urls");
+				
+				
+				collection.findOne({
+					"original": url
+				}, function(err, data){
+					if(err) throw err;
+					if(!data) { //if doesn't exist
+						//create url
+						console.log("Creating url");
+						data = "Some new data";
+					}
+					
+					temp = data;
+					
+				});
+				
+				
+				
+				db.close();
+			});
+			
 			//send json
 		} else {
 			console.log('not valid');
 			//if not valid send empty json/message
 		}
 		
-		res.end();
+		console.log(temp);
+		res.send(temp);
 		
 		
 	});
+	
+	  function validUrl(url) {
+	    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+	    return regex.test(url);
+	  }
 };
